112871ff38de67042487febdf5c20e4c51cb4c6f
skp81020-sys <skp81020@gmail.com>
2026-07-07 20:23:01 +0530

    Time: 0 ms (100%), Space: 46.7 MB (61.88%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void helper(int[][] image, int sr, int sc, int color,boolean vis[][],int orgiCol){
+        if(sr<0 || sc<0 || sr>=image.length || sc >=image[0].length || vis[sr][sc] || 
+        image[sr][sc]!=orgiCol){
+            return;
+        }
+        vis[sr][sc]=true;
+        image[sr][sc]=color;
+        // left
+        helper(image,sr,sc-1,color,vis,orgiCol);
+        // right
+         helper(image,sr,sc+1,color,vis,orgiCol);
+        // up
+         helper(image,sr-1,sc,color,vis,orgiCol);
+         // Down
+          helper(image,sr+1,sc,color,vis,orgiCol);
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][]=new boolean[image.length][image[0].length];
+         int  orgiCol=image[sr][sc];
+          helper(image,sr,sc,color,vis,orgiCol);
+          return image;
+    }
+}
