9f604fc7c0ba75ea54be714f855236d4d77ed997
skp81020-sys <skp81020@gmail.com>
2026-07-16 07:55:26 +0530

    Time: 33 ms (14.57%), Space: 47 MB (93.87%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int jump(int[] arr) {
+         int n=arr.length;
+
+        int dp[]=new int[n];
+        // Base case 
+       
+        for(int i=0;i<n-1;i++){
+            dp[i]=-1;
+        }
+         dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            int step=arr[i];
+            int ans=Integer.MAX_VALUE;
+            for(int j=i+1;j<=i+step && j<n ;j++){
+                if(dp[j]!=-1){
+                    ans =Math.min(ans, dp[j]+1);
+                }
+            }
+            if(ans!=Integer.MAX_VALUE){
+                dp[i]=ans;
+            }
+        }
+        return dp[0];
+    }
+}
