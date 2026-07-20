acf39e29bf1a1a1b6a0d06fb9fd3b4d56ae2f191
skp81020-sys <skp81020@gmail.com>
2026-07-20 23:14:10 +0530

    Time: 0 ms (100%), Space: 47.4 MB (34.2%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,16 +1,35 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int submax =Integer.MIN_VALUE;
-        int n=nums.length;
+    public int maxProduct(int[] arr) {
+          int ans=Integer.MIN_VALUE;
+        int n=arr.length;
+        int prefix=1;
+        int fllag=0;
+        // find out the prefix 
         for(int i=0;i<n;i++){
-            int ans=1;
-            for(int j=i;j<n;j++){
-                ans *=nums[j];
-                submax=Math.max(submax,ans);
+            if(arr[i]==0){
+                fllag=1;
+                prefix=1;
+            }else{
+                prefix *=arr[i];
+                ans =Math.max(ans, prefix);
             }
-            
+          
         }
-
-        return submax;
+        int sufix=1;
+        //find out the sufix of the Arrays
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]==0){
+                sufix=1;
+            }
+            else{
+                sufix *=arr[i];
+                ans =Math.max(ans, sufix);
+            }
+             
+        }
+        if(fllag ==1 && ans < 0){
+            return 0;
+        }
+        return ans;
     }
 }
