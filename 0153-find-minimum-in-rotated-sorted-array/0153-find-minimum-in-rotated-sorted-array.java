89935ae064b932f81fbf2360d8c118c0bde88830
skp81020-sys <skp81020@gmail.com>
2026-07-21 00:01:00 +0530

    Time: 0 ms (100%), Space: 43.6 MB (74.82%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -6,7 +6,7 @@ class Solution {
         // Not rotated Sorted the Arrays...
         if(nums[si]<=nums[ei]) return nums[si];
         // Rotated and sorted Arrays..
-        while(si <= ei){
+        while(si < ei){
              int mid =(si+ei)/2;
              if(nums[mid] < nums[ei]){
                 ei=mid;
@@ -15,6 +15,6 @@ class Solution {
                 si=mid+1;
              }
         }
-        return nums[ei];
+        return nums[si];
     }
 }
