c8e1239637446b7e4b73309e025acf5def55bf92
skp81020-sys <skp81020@gmail.com>
2026-07-21 22:09:29 +0530

    Time: 310 ms (14.91%), Space: 54.9 MB (99.22%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,39 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans =new ArrayList<>();
-        int n=nums.length;
-        HashSet<ArrayList<Integer>> s=new HashSet<>();
-        
+    public List<List<Integer>> threeSum(int[] arr) {
+         int n=arr.length;
+        Arrays.sort(arr);
+        ArrayList<List<Integer>> ans =new ArrayList<>();
         for(int i=0;i<n;i++){
-            HashSet<Integer> hs = new HashSet<>(); 
-            for(int j=i+1;j<n;j++){
-                    int numk=-(nums[i]+nums[j]);
-                    if(hs.contains(numk)){
-                        hs.remove(numk);
-                        ArrayList<Integer> arr=new ArrayList<>();
-                        arr.add(nums[i]);
-                        arr.add(nums[j]);
-                        arr.add(numk);
-                        Collections.sort(arr);
-                        if(!s.contains(arr)){
-                             ans.add(arr);
-                             s.add(arr);
-                        }
+            if(i > 0 && arr[i]==arr[i-1]) continue;
+            // by Usicg the two Pointer
+            int j=i+1;
+            int k=n-1;
+            while (j < k) {
+                if(arr[i]+arr[j]+arr[k]==0){
+                    ArrayList<Integer> a=new ArrayList<>();
+                    a.add(arr[i]);
+                    a.add(arr[j]);
+                    a.add(arr[k]);
+                    ans.add(a);
+                    j++;
+                    k--;
+                    while(j <k && arr[j]==arr[j-1]) {
+                        j++;
                     }
-                     hs.add(nums[j]);
+                }
+                else if(arr[i]+arr[j]+arr[k] > 0){
+                    k=k-1;
+                }
+                else{
+                    j=j+1;
+                }
             }
+        }
+        for(int i=0;i<ans.size();i++){
+            for(int j=0;j<ans.get(i).size();j++){
+                System.out.print(ans.get(i).get(j) +" ");
+            }
+            System.out.println();
         }
         return ans;
     }
