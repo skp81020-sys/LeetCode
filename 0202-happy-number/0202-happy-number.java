f82655540c2ecb9202c4713914ecff5a32f9d04b
skp81020-sys <skp81020@gmail.com>
2026-08-22 18:44:54 +0530

    Time: 1 ms (78.16%), Space: 42.2 MB (79.37%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int fun(int n){
+        int sum =0;
+        while(n > 0){
+            int r=n%10;
+            n=n/10;
+            sum +=r*r;
+        }
+
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+
+        while(fast !=1){
+            slow=fun(slow);
+            fast=fun(fast);
+            fast=fun(fast);
+
+            if(fast==slow && slow !=1){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
