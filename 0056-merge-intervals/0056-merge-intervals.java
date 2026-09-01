f39ccaff2672ad991d32e1069cf1329811a39609
Prajapati_Satish <skp81020@gmail.com>
2026-09-01 19:19:19 +0530

    Time: 10 ms (21.69%), Space: 48.6 MB (95.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
-        int n=arr.length;
-        ArrayList<ArrayList<Integer>> ans =new ArrayList<>();
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]); // sort the to the 1st number
+    public int[][] merge(int[][] nums) {
+        int n=nums.length;
+        int ans[][]=new int[n][2];
+        int a=0;
+       Arrays.sort(nums, (c, b) -> c[0] - b[0]);
         for(int i=0;i<n;i++){
-            int start=arr[i][0];
-            int end=arr[i][1];
-            while(i + 1 < n && end >= arr[i+1][0]){
-                end = Math.max(end, arr[i + 1][1]); 
+            int start=nums[i][0];
+            int end=nums[i][1];
+
+            while(i+1 < n && end >= nums[i+1][0]){
+                end=Math.max(end , nums[i+1][1]);
                 i++;
             }
-            
-           ArrayList<Integer> pair = new ArrayList<>();  // Bug 4 fix: proper ArrayList
-            pair.add(start);
-            pair.add(end);
-            ans.add(pair);
+            ans[a][0]=start;
+            ans[a][1]=end;
+            a++;
         }
-        n=ans.size();
-        int ans2[][]=new int[n][2];
-        for(int i=0;i<n;i++){
-            ans2[i][0]=ans.get(i).get(0);
-            ans2[i][1]=ans.get(i).get(1);
-        }
-        return ans2;
+
+        return Arrays.copyOf(ans, a);
     }
 }
