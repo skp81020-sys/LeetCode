fc383294216805ea05a6ddbbe69cd60e1d3e3b17
Prajapati_Satish <skp81020@gmail.com>
2026-09-05 16:55:13 +0530

    Time: 3 ms (13.91%), Space: 46.7 MB (94.29%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,48 +1,29 @@
 class Solution {
-    public int[][] insert(int[][] nums, int[] newInterval) {
-        int n = nums.length;
-        int[][] ans = new int[n + 1][2];
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n=intervals.length;
+        int nums[][]=new int[n+1][2];
 
-        int a = 0;
-        int s = newInterval[0];
-        int e = newInterval[1];
-
-        for (int i = 0; i < n; i++) {
-
-            int start = nums[i][0];
-            int end = nums[i][1];
-
-            // Current interval completely before newInterval
-            if (end < s) {
-                ans[a][0] = start;
-                ans[a][1] = end;
-                a++;
-            }
-
-            // Current interval overlaps newInterval
-            else if (start <= e) {
-                s = Math.min(s, start);
-                e = Math.max(e, end);
-            }
-
-            // Current interval completely after newInterval
-            else {
-                ans[a][0] = s;
-                ans[a][1] = e;
-                a++;
-
-                ans[a][0] = start;
-                ans[a][1] = end;
-                a++;
-
-                s = Integer.MAX_VALUE;
-            }
+        // Copy the intervals in nums and new intervals 
+        for(int i=0;i<n;i++){
+            nums[i][0]=intervals[i][0];
+            nums[i][1]=intervals[i][1];
         }
+        nums[n][0]=newInterval[0];
+        nums[n][1]=newInterval[1];
+        n=n+1;
+        int ans[][]=new int[n][2];
+        int a=0;
+        Arrays.sort(nums,(c,b) -> c[0]-b[0]);
+        for(int i=0;i<n;i++){
+            int s=nums[i][0];
+            int e=nums[i][1];
 
-        // Agar newInterval abhi insert nahi hua
-        if (s != Integer.MAX_VALUE) {
-            ans[a][0] = s;
-            ans[a][1] = e;
+            while(i+1 < n && e >=nums[i+1][0]){
+                e=Math.max(e,nums[i+1][1]);
+                i++;
+            }
+            ans[a][0]=s;
+            ans[a][1]=e;
             a++;
         }
 
