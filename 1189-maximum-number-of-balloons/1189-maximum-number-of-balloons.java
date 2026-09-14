05ea2b70e8cef52383dcd66efa14a6b25142cc04
Prajapati_Satish <skp81020@gmail.com>
2026-09-14 19:13:02 +0530

    Time: 12 ms (20.9%), Space: 44.3 MB (7.08%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,16 +1,26 @@
 class Solution {
-    public int maxNumberOfBalloons(String text) {
-        int arr[]=new int[256];
-        int n=text.length();
-
-        for(int i=0;i<n;i++){
-            char ch =text.charAt(i);
-            arr[ch]++;
+    public int maxNumberOfBalloons(String s) {
+        String target="balloon";
+         HashMap<Character,Integer> hs=new HashMap<>();
+         HashMap<Character,Integer> t=new HashMap<>();
+        if(target.length() > s.length()) return 0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            hs.put(ch,hs.getOrDefault(ch,0)+1);
         }
 
-        int ba= Math.min(arr['b'],arr['a']);
-        int lo=Math.min(arr['l']/2 , arr['o']/2);
+        for(int i=0;i<target.length();i++){
+            char ch=target.charAt(i);
+            t.put(ch,t.getOrDefault(ch,0)+1);
+        }
 
-        return Math.min(ba,Math.min(lo,arr['n']));
+        int ans =Integer.MAX_VALUE;
+        for(int i=0;i<target.length();i++){
+            char ch=target.charAt(i);
+            if (!hs.containsKey(ch)) return 0;
+            ans =Math.min(ans, hs.get(ch)/t.get(ch));
+        }
+
+        return ans;
     }
 }
