0f8eba60d6f0795a9f650ef840e66b69a1eac4d1
Prajapati_Satish <skp81020@gmail.com>
2026-09-17 12:31:47 +0530

    Time: 1 ms (37.39%), Space: 43.4 MB (8.26%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rearrangeCharacters(String s, String target) {
+        HashMap<Character,Integer> hs=new HashMap<>();
+         HashMap<Character,Integer> t=new HashMap<>();
+        if(target.length() > s.length()) return 0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            hs.put(ch,hs.getOrDefault(ch,0)+1);
+        }
+
+        for(int i=0;i<target.length();i++){
+            char ch=target.charAt(i);
+            t.put(ch,t.getOrDefault(ch,0)+1);
+        }
+
+        int ans =Integer.MAX_VALUE;
+        for(int i=0;i<target.length();i++){
+            char ch=target.charAt(i);
+            if (!hs.containsKey(ch)) return 0;
+            ans =Math.min(ans, hs.get(ch)/t.get(ch));
+        }
+
+        return ans;
+    }
+}
