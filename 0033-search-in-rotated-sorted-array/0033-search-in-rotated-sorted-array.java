7af0138fec4a9bed58efb6b85218dece53fa06d4
Prajapati_Satish <skp81020@gmail.com>
2026-09-19 19:05:23 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,22 @@
 class Solution {
-     public static int RotatedSortedArray(int arr[],int target,int si,int ei){
-        
-        while (si<= ei) {
-            int mid=si+(ei-si)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            // left Sorted 
-            if(arr[si] <= arr[mid]){
-                // Ab left ke liye 
-                if(arr[si] <= target && arr[mid] > target){
-                    // call left
-                     return RotatedSortedArray(arr, target, si, mid-1);
-                }
-                else{
-                    // Call Right
-                    return RotatedSortedArray(arr, target, mid+1, ei);
-                }
-            }
-            else{
-                if(arr[mid] < target && arr[ei] >= target){
-                    return   RotatedSortedArray(arr, target, mid+1, ei);
-                }
-                else{
-                   return  RotatedSortedArray(arr, target, si, mid-1);
-                }
-            }
+    public int search(int[] nums, int target) {
+        int lo=0;
+        int hi=nums.length-1;
+        while(lo <= hi ){
+            int mid=lo +(hi-lo)/2;
+            if(nums[mid]==target) return mid;
 
+            if(nums[mid] < nums[hi]){
+                if(nums[mid] < target && target <= nums[hi]){
+                    lo=mid+1;
+                }else{
+                    hi=mid-1;
+                }
+            }else{
+                if(nums[lo] <= target && target < nums[mid])     hi=mid-1;
+                else lo=mid+1;
+            }
         }
         return -1;
-    }
-    public int search(int[] nums, int target) {
-        return RotatedSortedArray(nums,target, 0, nums.length-1);
     }
 }
